d7573c33d669faad03685f13b36386c64d850858
Zack Wang <ttwang@gmail.com>
2020-01-05 13:34:43 +0800

    Parameter inputs are devfs-string and i2c-address-int

--- HDC1080.go
+++ HDC1080.go
@@ -2,20 +2,35 @@ package hdc1080
 import(
   "log"
   "time"
+  "errors"
 	"golang.org/x/exp/io/i2c"
 )
 
-func ReadTempHumi(d *i2c.Device)(float32,float32,error){
+func ReadTempHumi(d string,a int)(float32,float32,error){
 // 0x40 is TI HDC1080
+  b2:=[]byte{0x0,0x0}
+  b4:=[]byte{0x0,0x0,0x0,0x0}
   t:=float32(0.0)
   h:=float32(0.0)
-  err=d.ReadReg(0xFE,b2)
+  Dev,err:=i2c.Open(&i2c.Devfs{Dev:d},a)
+  defer Dev.Close()
+
   if err!=nil{
-    log.Println("0:",err)
-    return 0.0,0,err
+    log.Println("I2C BUS Error")
+    return 0.0,0.0,err
   }else{
-    log.Println("vendor=",string(b2))
-  }
+
+    err:=Dev.ReadReg(0xFE,b2)
+    if err!=nil{
+      log.Println("0:",err)
+      return 0.0,0.0,err
+    }else{
+      vendor:=string(b2)
+      //log.Println("vendor=",vendor)
+      if vendor!="TI"{
+        return t,h,errors.New("Not TI HDC1080")
+      }
+    }
   /*
   err=d.ReadReg(0x02,b2)
   if err!=nil{
@@ -26,22 +41,21 @@ func ReadTempHumi(d *i2c.Device)(float32,float32,error){
   }
   */
   // get H & T
-  i2c1.Write([]byte{0x00})
-  // time.Sleep(10 * time.Millisecond)
-  b4:=[]byte{0x00,0x00,0x00,0x00}
-  for n:=0;n<10;n++{
-    err=i2c1.Read(b4)
-    if err==nil{
-      log.Println("Data=",b4)
-      t16:=int(b4[0])<<8 + int(b4[1])
-      h16:=int(b4[2])<<8 + int(b4[3])
-      t=165.0*(float32(t16)/65536.0)-40.0
-      h=(float32(h16)/65536)*100.0
-      log.Println("Temp=",t," Humidity=",h,"%")
-      break
+    Dev.Write([]byte{0x00})
+    for n:=0;n<10;n++{
+      err=Dev.Read(b4)
+      if err==nil{
+        //log.Println("Data=",b4)
+        t16:=int(b4[0])<<8 + int(b4[1])
+        h16:=int(b4[2])<<8 + int(b4[3])
+        t=165.0*(float32(t16)/65536.0)-40.0
+        h=(float32(h16)/65536)*100.0
+        log.Println("Temp=",t," Humidity=",h,"%")
+        break
+      }
+      time.Sleep(100 * time.Millisecond)
     }
-    time.Sleep(100 * time.Millisecond)
+    // if BREAK, err should be nil. If NOT, No Data
+    return t,h,err
   }
-  // if BREAK, err should be nil. If NOT, No Data 
-  return t,h,err
 }
